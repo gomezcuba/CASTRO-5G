4ef16543fe59fa27a3a06c2bafd5b01081006837
fgomez <gomezcuba>
2021-11-10 14:56:48 +0100

    Added CS estimation module

---
+++ OMPCachedRunner.py
@@ -0,0 +1,278 @@
+import numpy as np
+import collections as col
+
+OMPInfoSet = col.namedtuple( "OMPInfoSet",[
+        "coefs",
+        "delays",
+        "AoDs",
+        "AoAs",
+        "observations",
+        "outputs",
+    ])
+
+class OMPCachedRunner:
+    def __init__(self, preloadDics = []):
+        self.cachedDics={}
+
+    def createOutDicCols(self, delays,sinaods,sinaoas,Nt,Nd,Na):
+        vout=np.empty(shape=(Nt*Nd*Na,len(delays)),dtype=np.cdouble)
+        for i in range(len(delays)):
+            vt=np.fft.fft(np.sinc( np.arange(0.0,Nt,1.0).reshape(Nt,1,1) - delays[i] ),axis=0)/np.sqrt(1.0*Nt)
+            va=np.exp( -1j*np.pi * np.arange(0.0,Na,1.0).reshape(1,Na,1) * sinaoas[i] ) /np.sqrt(1.0*Na)
+            vd=np.exp( -1j*np.pi * np.arange(0.0,Nd,1.0).reshape(1,1,Nd) * sinaods[i] ) /np.sqrt(1.0*Nd)
+            vall=(vt*va*vd)
+            vall=vall/np.sqrt(np.sum(np.abs(vall)**2)) #if vectors are not normalized, correlation-comparison is not valid
+            vout[:,i]=vall.reshape((Nt*Nd*Na,))
+        return( vout )
+    def createDic(self, Nt,Nd,Na,Xt,Xd,Xa):
+        Tdic=np.arange(0.0,Nt,1.0/Xt)
+        Ddic=np.arange(-1.0,1.0,2.0/Nd/Xd)
+        Adic=np.arange(-1.0,1.0,2.0/Na/Xa)
+        outputDic=np.empty(shape=(Nt*Nd*Na,np.size(Tdic)*np.size(Ddic)*np.size(Adic)),dtype=np.cdouble)
+        ctr=0
+#        for t in range(np.size(Tdic)):
+#            for d in  range(np.size(Ddic)):
+#                for a in  range(np.size(Adic)):
+#                    outputDic[:,ctr]=self.createOutDicCols([Tdic[t]],[Ddic[d]],[Adic[a]],Nt,Nd,Na).reshape((Nt*Nd*Na,))
+#                    ctr+=1
+        outputDic=self.createOutDicCols(
+                np.tile( Tdic.reshape(np.size(Tdic),1,1) , (1,np.size(Ddic),np.size(Adic)) ).reshape((np.size(Tdic)*np.size(Ddic)*np.size(Adic),)),
+                np.tile( Ddic.reshape(1,np.size(Ddic),1) , (np.size(Tdic),1,np.size(Adic)) ).reshape((np.size(Tdic)*np.size(Ddic)*np.size(Adic),)),
+                np.tile( Adic.reshape(1,1,np.size(Adic)) , (np.size(Tdic),np.size(Ddic),1) ).reshape((np.size(Tdic)*np.size(Ddic)*np.size(Adic),)),
+                Nt,Nd,Na)
+        result = OMPInfoSet(None,Tdic,Ddic,Adic,{},outputDic)
+        self.cachedDics[ (Nt,Nd,Na,Xt,Xd,Xa) ] = result
+        return(result)
+    def createObsDicCols(self, vout,vp,wp ):
+        Ncols=np.shape(vout)[1]
+        Nt=np.shape(vp)[0]
+        Nxp=np.shape(vp)[1]
+        Nd=np.shape(vp)[2]
+#        Nrft=np.shape(vp)[3]
+        Nrfr=np.shape(wp)[2]
+        Na=np.shape(wp)[3]
+        vresult=np.empty(shape=(Nt*Nxp*Nrfr,Ncols),dtype=np.cdouble)
+        for c in range(Ncols):
+#            vtest=vout[:,c].reshape(Nt,1,Na,Nd)
+#            vref=np.empty(shape=(Nt,Nxp,Nrfr),dtype=np.cdouble)
+#            for k in range(0,Nt):
+#                for p in range(0,Nxp):
+#                    vref[k,p,:]=np.matmul(wp[k,p,:,:] , np.sum(np.matmul( vtest[k,:,:] ,vp[k,p,:,:]),axis=1,keepdims=True) ).reshape((Nrfr,))
+            vref=np.matmul( wp,  np.sum( np.matmul( vout[:,c].reshape(Nt,1,Na,Nd) ,vp) ,axis=3,keepdims=True) )
+            vresult[:,c]= vref.reshape((Nt*Nxp*Nrfr,))
+        return( vresult )
+    def createObsDic(self, pilotsID,vp,wp,Xt,Xd,Xa):
+        Nt=np.shape(vp)[0]
+#        Nxp=np.shape(vp)[1]
+        Nd=np.shape(vp)[2]
+#        Nrft=np.shape(vp)[3]
+#        Nrfr=np.shape(wp)[2]
+        Na=np.shape(wp)[3]
+        if (Nt,Nd,Na,Xt,Xd,Xa) not in self.cachedDics:
+            paramDic=self.createDic(Nt,Nd,Na,Xt,Xd,Xa)
+        else:
+            paramDic = self.cachedDics[(Nt,Nd,Na,Xt,Xd,Xa)]
+        observDic=self.createObsDicCols( paramDic.outputs ,vp,wp)
+        self.cachedDics[(Nt,Nd,Na,Xt,Xd,Xa)].observations[pilotsID]=observDic
+        return(observDic)
+        
+        
+    def freeCacheOfPilot(self,pilotsID,Nt,Nd,Na,Xt,Xd,Xa):
+        if (Nt,Nd,Na,Xt,Xd,Xa) in self.cachedDics:
+            if pilotsID in self.cachedDics[(Nt,Nd,Na,Xt,Xd,Xa)].observations:
+                self.cachedDics[(Nt,Nd,Na,Xt,Xd,Xa)].observations.pop(pilotsID)  
+                
+    def OMPBR(self,v,xi,pilotsID,vp,wp, Xt=1.0, Xd=1.0, Xa=1.0, Xmu=1.0):
+        Nt=np.shape(v)[0]
+        Nxp=np.shape(vp)[1]
+        Nd=np.shape(vp)[2]
+#        Nrft=np.shape(vp)[3]
+        Nrfr=np.shape(wp)[2]
+        Na=np.shape(wp)[3]
+        vflat=v.reshape(Nt*Nxp*Nrfr,1)
+        r=vflat
+        et=1j*np.zeros(np.shape(r))
+    
+        if (Nt,Nd,Na,Xt,Xd,Xa) not in self.cachedDics:
+            dicParams=self.createDic(Nt,Nd,Na,Xt,Xd,Xa)
+        else:
+            dicParams = self.cachedDics[(Nt,Nd,Na,Xt,Xd,Xa)]
+        if pilotsID not in dicParams.observations:
+            observDic=self.createObsDic(pilotsID,vp,wp,Xt,Xd,Xa)
+        else:
+            observDic=dicParams.observations[pilotsID]
+    
+        Tdic=dicParams.delays
+        Ddic=dicParams.AoDs
+        Adic=dicParams.AoAs
+#        outputDic=dicParams.outputs 
+        observDicConj=observDic.transpose().conj()#cache this value for speed
+    
+        Rsupp=np.zeros(shape=(Nt*Nxp*Nrfr,Nt*Nxp*Nrfr),dtype=np.complex)
+        delay_supp=np.zeros(Nt*Nxp*Nrfr)
+        aod_supp=np.zeros(Nt*Nxp*Nrfr)
+        aoa_supp=np.zeros(Nt*Nxp*Nrfr)
+        #Isupp=OMPInfoSet([],[],[],[],[],[])
+        et=np.empty(shape=np.shape(r))
+        ctr=0        
+        if Xmu>1.0:#pregenerate this only once before the loop
+            boolTable=np.array([ [(x%(2**(n+1)))//(2**n) for n in range(3)] for x in range(8)],dtype=np.double) #[0,0,0],[1,0,0],[0,1,0]...[1,1,1]
+            muTableInit=boolTable-.5 #[-.5,-.5,-.5] ... [.5,.5,.5]
+        while ((ctr<Nt*Nxp*Nrfr) and (np.sum(np.abs(r)**2)>xi)) or ctr==0:
+            c=np.matmul( observDicConj , r )
+            ind=np.argmax(np.abs(c))
+    
+            t=int(np.floor(ind/np.size(Ddic)/np.size(Adic)))
+            d=int(np.mod(np.floor(ind/np.size(Adic)),np.size(Ddic)))
+            a=int(np.mod(ind,np.size(Adic)))
+            # print((t,d,a))
+            if Xmu>1.0:
+                muTable=muTableInit
+                mid_mu=np.array([0,0,0],dtype=np.double)
+                while ( np.any(np.abs(mid_mu-muTable)>(.5/Xmu)) ):
+                    vall=self.createOutDicCols( Tdic[t] + muTable[:,0]/Xt , np.mod( Ddic[d] - muTable[:,2]*2.0/Nd/Xd +1,2)-1 , np.mod( Adic[a] - muTable[:,1]*2.0/Na/Xa +1,2)-1 ,Nt,Nd,Na)
+                    vref=self.createObsDicCols(vall,vp,wp)
+                    corr_mu=np.matmul( vref.transpose().conj() , r )
+                    bestInd=np.argmax(np.abs(corr_mu))
+                    best_mu=muTable[bestInd,:]
+                    muTable=mid_mu*(1-boolTable)+best_mu*boolTable #[mid_mu,mid_mu,mid_mu] ... [best_mu,best_mu,best_mu]
+                    mid_mu=np.mean(muTable,axis=0)
+                mu=mid_mu
+                vall=self.createOutDicCols( [Tdic[t] + mu[0]/Xt] , [Ddic[d] -mu[2]*2.0/Nd/Xd] , [Adic[a] -mu[1]*2.0/Na/Xa] ,Nt,Nd,Na)
+                vref=self.createObsDicCols(vall,vp,wp)
+                if ( np.abs(c[ind]) < np.abs(np.sum(vref.conj()*r)) ):
+                    Rsupp[:,ctr]    = vref.reshape((Nt*Nxp*Nrfr,))
+                    delay_supp[ctr] = Tdic[t] + mu[0]/Xt
+                    aod_supp[ctr]   = np.mod( Ddic[d] -mu[2]*2.0/Nd/Xd +1,2)-1
+                    aoa_supp[ctr]   = np.mod( Adic[a] -mu[1]*2.0/Na/Xa +1,2)-1
+#                    Rsupp=np.concatenate(( Rsupp, vref ), axis=1)
+#                    Isupp.delays      .append( Tdic[t] + mu[0]/Xt )
+#                    Isupp.AoDs        .append( Ddic[d] -mu[2]*2.0/Nd/Xd )
+#                    Isupp.AoAs        .append( Adic[a] -mu[1]*2.0/Na/Xa )                
+                else:
+                    # print("Autopatched correlation decrease")                    
+                    Rsupp[:,ctr]    = observDic[:,ind]
+                    delay_supp[ctr] = Tdic[t]
+                    aod_supp[ctr]   = Ddic[d]
+                    aoa_supp[ctr]   = Adic[a]
+#                    Rsupp=np.concatenate(( Rsupp, observDic[:,ind].reshape(Nt*Nxp*Nrfr,1) ), axis=1)
+#                    Isupp.delays      .append( Tdic[t] )
+#                    Isupp.AoDs        .append( Ddic[d] )
+#                    Isupp.AoAs        .append( Adic[a] )
+            else:      
+                Rsupp[:,ctr]    = observDic[:,ind]
+                delay_supp[ctr] = Tdic[t]
+                aod_supp[ctr]   = Ddic[d]
+                aoa_supp[ctr]   = Adic[a]
+#            et=np.matmul(Rsupp, np.linalg.lstsq(Rsupp,vflat,rcond=None)[0] )
+            vflat_proj,_,_,_=np.linalg.lstsq(Rsupp[:,0:ctr+1],vflat,rcond=None)
+#            vflat_proj=self.lsqNumbaWrapper(Rsupp,vflat)
+            et=np.matmul(Rsupp[:,0:ctr+1], vflat_proj )
+            r=vflat-et
+            # print(np.sum(np.abs(r)**2))
+            # print('OMPBR ctr %d'%ctr)
+            ctr=ctr+1
+#        print('OMPBR ctr %d'%ctr)        
+        Isupp=OMPInfoSet(vflat_proj,delay_supp[0:ctr] , np.arcsin(np.clip( aod_supp[0:ctr], -1,1)) , np.arcsin(np.clip( aoa_supp[0:ctr], -1,1)) , Rsupp[:,0:ctr] , self.createOutDicCols( delay_supp[0:ctr] , aod_supp[0:ctr] , aoa_supp[0:ctr] , Nt,Nd,Na) )
+        hest=np.matmul(Isupp.outputs, vflat_proj )
+        return( hest.reshape(Nt,Na,Nd), Isupp )
+        
+    def Shrinkage(self,v,shrinkageParams,Nit,pilotsID,vp,wp, Xt=1.0, Xd=1.0, Xa=1.0, shrinkageAlg = "ISTA"):
+        Nt=np.shape(v)[0]
+        Nxp=np.shape(vp)[1]
+        Nd=np.shape(vp)[2]
+#        Nrft=np.shape(vp)[3]
+        Nrfr=np.shape(wp)[2]
+        Na=np.shape(wp)[3]
+        vflat=v.reshape(Nt*Nxp*Nrfr,1)
+        r=vflat
+    
+        if (Nt,Nd,Na,Xt,Xd,Xa) not in self.cachedDics:
+            dicParams=self.createDic(Nt,Nd,Na,Xt,Xd,Xa)
+        else:
+            dicParams = self.cachedDics[(Nt,Nd,Na,Xt,Xd,Xa)]
+        if pilotsID not in dicParams.observations:
+            observDic=self.createObsDic(pilotsID,vp,wp,Xt,Xd,Xa)
+        else:
+            observDic=dicParams.observations[pilotsID]
+    
+        Tdic=dicParams.delays
+        Ddic=dicParams.AoDs
+        Adic=dicParams.AoAs
+        outputDic=dicParams.outputs
+        
+#        print("max beta %f"%(1.0/np.sum(np.abs(outputDic)**2)))
+    
+        Rsupp=np.empty(shape=(Nt*Nxp*Nrfr,0))
+        Isupp=OMPInfoSet([],[],[],[],[])
+        et=1j*np.zeros((np.shape(observDic)[1],1))
+        if  shrinkageAlg == "FISTA":
+            old_et=et
+        if  shrinkageAlg == "VAMP":
+            A=observDic
+            (U,S,Vh)=np.linalg.svd(A)
+            M1=(S*U).T.conj()               #faster implementation of np.matmul(np.diag(S),U.T.conj())
+            r_mmse=r
+            sigma2w=shrinkageParams[0]
+            sigma2t_mmse=sigma2w
+            alpha=shrinkageParams[1]
+        for itctr in range(Nit):
+            if shrinkageAlg == "FISTA":
+                lamb=shrinkageParams[0]
+                beta=shrinkageParams[1]
+                c=et+beta*np.matmul( observDic.transpose().conj() , r )+(et-old_et)*(itctr-2.0)/(itctr+1)
+                old_et=et
+                et=np.exp(1j*np.angle(c))*np.maximum(np.abs(c)-lamb,0)
+                r=vflat-np.matmul(observDic,et)
+            elif shrinkageAlg == "ISTA":
+                lamb=shrinkageParams[0]
+                beta=shrinkageParams[1]
+                c=et+beta*np.matmul( observDic.transpose().conj() , r )
+                et=np.exp(1j*np.angle(c))*np.maximum(np.abs(c)-lamb,0)
+                r=vflat-np.matmul(observDic,et)
+            elif  shrinkageAlg == "AMP":
+                alpha=shrinkageParams[0]
+                bt=np.sum(np.abs(et)>0)/(Nt*Nxp*Nrfr)
+                c=et+np.matmul( observDic.transpose().conj() , r )
+                lamb = alpha*np.sqrt(np.sum(np.abs(r)**2)/(Nt*Nxp*Nrfr))
+                et=np.exp(1j*np.angle(c))*np.maximum(np.abs(c)-lamb,0)
+                old_r=r
+                r=vflat-np.matmul(observDic,et)+bt*old_r
+            elif  shrinkageAlg == "VAMP":
+                #LMMSE stage
+                M2=sigma2w/sigma2t_mmse*Vh
+#                M3=np.diag(S**2+sigma2w/sigma2t_mmse)
+#                if np.any(np.isnan(np.matmul(M1,vflat)+np.matmul(M2,r_mmse))) or  np.any(np.isinf(np.matmul(M1,vflat)+np.matmul(M2,r_mmse))):
+#                    print("aqui")
+#                et_mmse=np.matmul(Vh.T.conj(),np.linalg.lstsq(M3,np.matmul(M1,vflat)+np.matmul(M2,r_mmse),rcond=None)[0])
+                M3_diagInv=( 1/(S**2+sigma2w/sigma2t_mmse) ).reshape((Nt*Nd*Na,1))
+                et_mmse=np.matmul(Vh.T.conj(), M3_diagInv * np.matmul(M1,vflat)+np.matmul(M2,r_mmse) )
+                vt_mmse=np.sum(1.0/((S**2)*sigma2t_mmse/sigma2w+1))/(Nt*Nd*Na)
+                r=et_mmse-vt_mmse*r_mmse//(1-vt_mmse)
+                sigma2t=sigma2t_mmse*vt_mmse/(1-vt_mmse)
+                #Shrinkage stage
+                lamb=alpha*np.sqrt(sigma2t/(Nt*Nxp*Nrfr))
+                et=np.exp(1j*np.angle(r))*np.maximum(np.abs(r)-lamb,0)
+                vt=np.sum(np.abs(et)>0)/(Nt*Nxp*Nrfr)
+                r_mmse=(et-vt*r)/(1-vt)
+                sigma2t_mmse=sigma2t*vt/(1-vt)
+            else:
+                print("Unknown algoritm")
+                return((0,0))
+                    
+#            et=np.matmul(Rsupp, np.linalg.lstsq(Rsupp,vflat,rcond=None)[0] )
+        lInds=np.where(et>0)[0]        
+        Rsupp=np.empty(shape=(Nt*Nxp*Nrfr,0))
+        Isupp=OMPInfoSet([],[],[],[],[])
+        for ind in lInds:
+            t=int(np.floor(ind/np.size(Ddic)/np.size(Adic)))
+            d=int(np.mod(np.floor(ind/np.size(Adic)),np.size(Ddic)))
+            a=int(np.mod(ind,np.size(Adic)))
+            Rsupp=np.concatenate(( Rsupp, observDic[:,ind].reshape(Nt*Nxp*Nrfr,1) ), axis=1)
+            Isupp.delays      .append( Tdic[t] )
+            Isupp.AoDs        .append( np.arcsin(Ddic[d]) )
+            Isupp.AoAs        .append( np.arcsin(Adic[a]) )
+        Isupp=OMPInfoSet(Isupp.delays , Isupp.AoDs , Isupp.AoAs , Rsupp , self.createOutDicCols( Isupp.delays , Isupp.AoDs , Isupp.AoAs ,Nt,Nd,Na) )
+        hest=np.matmul(outputDic, et )
+        return( hest.reshape(Nt,Na,Nd), Isupp )
+            
---
+++ testOMPBRmodule.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python
+
+import threeGPPMultipathGenerator as pg
+import multipathChannel as ch
+import OMPCachedRunner as oc
+import MIMOPilotChannel as pil
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import time
+from progress.bar import Bar
+
+plt.close('all')
+
+Nchan=100
+Nd=4
+Na=2
+Nt=4
+Nxp=4
+Nrft=1
+Nrfr=2
+K=512
+Ts=2.5
+Ds=Ts*Nt
+SNRs=10**(np.arange(-1,2.01,1.0))
+#SNRs=10**(np.arange(1,1.01,1.0))
+MSE=[]
+Npaths=[]
+totTimes=[]
+
+def pseudoOMP(v,xi):
+    #assumes v is explicitly sparse and returns its N largest coefficients where rho(N)<xi
+    (Nt,Nd,Na)=np.shape(v)
+    r=v
+    et=1j*np.zeros(np.shape(r))
+    #at least one iteration
+    ind=np.unravel_index(np.argmax(np.abs(r)),np.shape(r))
+    et[ind]=r[ind]
+    r=v-et
+    ctr=1
+    while np.sum(np.abs(r)**2)>xi:
+        ind=np.unravel_index(np.argmax(np.abs(r)),np.shape(r))
+        et[ind]=r[ind]
+        r=v-et
+        ctr=ctr+1
+#    print('OMP ctr %d'%ctr)
+    e=et
+    return(e)
+
+def pseudoISTA(v,xi,Niter,horig):
+    #assumes v is explicitly sparse and returns its N largest coefficients where rho(N)<xi
+    (Nt,Nd,Na)=np.shape(v)
+    r=v
+    et=np.zeros(np.shape(r))
+    beta=.5
+    for n in range(Niter):
+        c=et+beta*r
+        et=np.exp(1j*np.angle(c))*np.maximum(np.abs(c)-xi,0)
+        r=v-et
+#        print(np.mean(np.abs(et-horig)**2)/np.mean(np.abs(horig)**2))
+    e=et
+    return(e)
+
+def pseudoFISTA(v,xi,Niter,horig):
+    #assumes v is explicitly sparse and returns its N largest coefficients where rho(N)<xi
+    (Nt,Nd,Na)=np.shape(v)
+    r=v
+    et=np.zeros(np.shape(r))
+    old_et=et
+    beta=.5
+    for n in range(Niter):
+        c=et+beta*r+(et-old_et)*(n-2.0)/(n+1.0)
+        old_et=et
+        et=np.exp(1j*np.angle(c))*np.maximum(np.abs(c)-xi,0)
+        r=v-et
+#        print(np.mean(np.abs(et-horig)**2)/np.mean(np.abs(horig)**2))
+    e=et
+    return(e)
+
+def pseudoAMP(v,xi,Niter,horig):
+    #assumes v is explicitly sparse and returns its N largest coefficients where rho(N)<xi
+    (Nt,Nd,Na)=np.shape(v)
+    r=v
+    et=np.zeros(np.shape(r))
+    old_r=r
+    for n in range(Niter):
+        bt=np.sum(np.abs(et)>0)/(Nt*Nd*Na)
+        c=et+r
+        ldt = xi*np.sqrt(np.sum(np.abs(r)**2)/(Nt*Nd*Na))
+        et=np.exp(1j*np.angle(c))*np.maximum(np.abs(c)-ldt,0)
+        old_r=r
+        r=v-et+bt*old_r
+#        print(np.mean(np.abs(et-horig)**2)/np.mean(np.abs(horig)**2))
+    e=et
+    return(e)
+
+omprunner = oc.OMPCachedRunner()
+pilgen = pil.MIMOPilotChannel("Rectangular")
+probgen = prb.csProblemGenerator(Nt,Nd,Na,Nrft,Nrfr,Nxp,Ts,"Rectangular")
+probgen.pregenerate(Nchan)
+
+legStrAlgs=[
+        'LS',
+        'pseudoOMP',
+        'OMPx1',
+        'OMPx2',
+        'OMPBR',
+#        'pseudoISTA',
+#        'ISTAx1',
+#        'pseudoFISTA',
+#        'FISTAx1',
+#        'pseudoAMP',
+#        'AMP',
+#        'VAMP'
+        ]
+
+bar = Bar("CS sims", max=Nchan)
+bar.check_tty = False
+for ichan in range(0,Nchan):
+    (hk,zp,wp,vp)=probgen.listPreparedProblems[ichan]
+
+    h=np.fft.ifft(hk,axis=0)
+    hsparse=np.fft.ifft(h,axis=1)*np.sqrt(Nd)
+    hsparse=np.fft.ifft(hsparse,axis=2)*np.sqrt(Na)
+    # print(hk)
+    MSE.append([])
+    totTimes.append([])
+    Npaths.append([])
+
+    zh=probgen.zAWGN((Nt,Na,Nd))
+
+    for isnr in range(0,len(SNRs)):
+        sigma2=1.0/SNRs[isnr]
+
+        #direct observation with noise
+        hest=hsparse*np.sqrt(Nt)+zh*np.sqrt(sigma2)
+        MSE[-1].append([])
+        MSE[-1][-1].append(np.mean(np.abs(hsparse-hest/np.sqrt(Nt))**2)/np.mean(np.abs(hsparse)**2))
+
+        #pseudoOMP sparse component selection
+        t0 = time.time()
+        hest2=pseudoOMP(hest,sigma2*Nt*Na*Nd)
+        MSE[-1][-1].append(np.mean(np.abs(hsparse-hest2/np.sqrt(Nt))**2)/np.mean(np.abs(hsparse)**2))
+        totTimes[-1].append([])
+        totTimes[-1][-1].append(time.time()-t0)
+        Npaths[-1].append([])
+        Npaths[-1][-1].append(np.sum(hest2!=0))
+
+        yp=pilgen.applyPilotChannel(hk,wp,vp,zp*np.sqrt(sigma2))
+        t0 = time.time()
+        hest3,paths=omprunner.OMPBR(yp,sigma2*Nt*Nxp*Nrfr,ichan,vp,wp,1.0,1.0,1.0,1.0)
+        MSE[-1][-1].append(np.mean(np.abs(hk-hest3)**2)/np.mean(np.abs(hk)**2))
+        totTimes[-1][-1].append(time.time()-t0)
+        Npaths[-1][-1].append(len(paths.delays))
+
+        t0 = time.time()
+        hest4,paths=omprunner.OMPBR(yp,sigma2*Nt*Nxp*Nrfr,ichan,vp,wp,2.0,2.0,2.0,1.0)
+        MSE[-1][-1].append(np.mean(np.abs(hk-hest4)**2)/np.mean(np.abs(hk)**2))
+        totTimes[-1][-1].append(time.time()-t0)
+        Npaths[-1][-1].append(len(paths.delays))
+
+        t0 = time.time()
+        hest5,paths=omprunner.OMPBR(yp,sigma2*Nt*Nxp*Nrfr,ichan,vp,wp,1.0,1.0,1.0,10.0)
+        MSE[-1][-1].append(np.mean(np.abs(hk-hest5)**2)/np.mean(np.abs(hk)**2))
+        totTimes[-1][-1].append(time.time()-t0)
+        Npaths[-1][-1].append(len(paths.delays))
+#
+#        t0 = time.time()
+#        hest6=pseudoISTA(hest,.5*np.sqrt(sigma2),15,hsparse*np.sqrt(Nt))
+#        MSE[-1][-1].append(np.mean(np.abs(hsparse-hest6/np.sqrt(Nt))**2)/np.mean(np.abs(hsparse)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(np.sum(hest6!=0))
+
+#        t0 = time.time()
+#        hest7,paths=omprunner.Shrinkage(yp, (.5*np.sqrt(sigma2), .5) ,15,ichan,vp,wp,1.0,1.0,1.0,'ISTA')
+#        MSE[-1][-1].append(np.mean(np.abs(hk-hest7)**2)/np.mean(np.abs(hk)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(len(paths.delays))
+#
+#        t0 = time.time()
+#        hest8,paths=omprunner.Shrinkage(yp, (.5*np.sqrt(sigma2), .5) ,15,ichan,vp,wp,2.0,2.0,2.0,'ISTA')
+#        MSE[-1][-1].append(np.mean(np.abs(hk-hest8)**2)/np.mean(np.abs(hk)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(len(paths.delays))
+
+#        t0 = time.time()
+#        hest9=pseudoFISTA(hest,.5*np.sqrt(sigma2),15,hsparse*np.sqrt(Nt))
+#        MSE[-1][-1].append(np.mean(np.abs(hsparse-hest9/np.sqrt(Nt))**2)/np.mean(np.abs(hsparse)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(np.sum(hest6!=0))
+#
+#        t0 = time.time()
+#        hest10,paths=omprunner.Shrinkage(yp, (.5*np.sqrt(sigma2),.5),15,ichan,vp,wp,1.0,1.0,1.0,'FISTA')
+#        MSE[-1][-1].append(np.mean(np.abs(hk-hest10)**2)/np.mean(np.abs(hk)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(len(paths.delays))
+#
+#        t0 = time.time()
+#        hest11=pseudoAMP(hest,.5*np.sqrt(sigma2),15,hsparse*np.sqrt(Nt))
+#        MSE[-1][-1].append(np.mean(np.abs(hsparse-hest11/np.sqrt(Nt))**2)/np.mean(np.abs(hsparse)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(np.sum(hest6!=0))
+#
+#        t0 = time.time()
+#        hest12,paths=omprunner.Shrinkage(yp,(.5*np.sqrt(sigma2),),15,ichan,vp,wp,1.0,1.0,1.0,'AMP')
+#        MSE[-1][-1].append(np.mean(np.abs(hk-hest12)**2)/np.mean(np.abs(hk)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(len(paths.delays))
+#
+#        t0 = time.time()
+#        hest13,paths=omprunner.Shrinkage(yp,(sigma2,5),5,ichan,vp,wp,1.0,1.0,1.0,'VAMP')
+#        MSE[-1][-1].append(np.mean(np.abs(hk-hest13)**2)/np.mean(np.abs(hk)**2))
+#        totTimes[-1][-1].append(time.time()-t0)
+#        Npaths[-1][-1].append(len(paths.delays))
+    bar.next()
+bar.finish()
+
+MSE=np.array(MSE)
+totTimes=np.array(totTimes)
+print(np.mean(MSE,axis=0))
+print(np.mean(totTimes,axis=0))
+plt.semilogy(10*np.log10(SNRs),np.mean(MSE,axis=0))
+plt.legend(legStrAlgs)
+plt.xlabel('SNR(dB)')
+plt.ylabel('MSE')
+plt.figure()
+plt.bar(range(np.size(totTimes)//Nchan),np.mean(totTimes,axis=0).reshape(np.size(totTimes)//Nchan,))
+plt.xlabel('alg')
+plt.ylabel('runtime')
+plt.figure()
+plt.bar(range(np.size(totTimes)//Nchan),np.mean(Npaths,axis=0).reshape(np.size(Npaths)//Nchan,))
+plt.xlabel('alg')
+plt.ylabel('N paths')
+plt.show()
