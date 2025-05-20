cc37c491daf787c5d8a3540eb6c880b1a38f8ae3
Daniel Guarino <danielguarino.universidad.com>
2025-05-20 13:09:56 +0200

    Minor changes

--- Drafts/testDMRS.py
+++ Drafts/testDMRS.py
@@ -12,22 +12,37 @@ import sys
 sys.path.append('../')
 from CASTRO5G import multipathChannel as mc
 plt.close('all')
-Nrft=1
-Nrfr=2
+
 Nd=2
 Na=2
+Nrft=1
+Nrfr=2 # Tienes que poner aqui Na cuando uses wp matrices identidad
 Nsym=5
 K=64
+
 # Lista de pilotos a comparar
 pilots_config = [
     ("DMRS", "DMRS")
 ]
-M_PSK=2
+
+algConfig={
+    "M":2,
+    "MappingType": 'A',
+    "SymbolAllocation": (0, 14),
+    "DMRSConfigurationType": 1,
+    "DMRSLength": 1,
+    "DMRSAdditionalPosition": 0,
+    "DMRSTypeAPosition": 2,
+    "NIDNSCID": 10,
+    "NSCID": 0,
+    "NSizeSymbol": 1
+    }
+
 for ipilot, (pilot_name, pilot_alg) in enumerate(tqdm(pilots_config, desc="Pilot Types: ")):
-    pilgen = mc.MIMOPilotChannel(pilot_alg,M_PSK,Nsym,Nd)
+    pilgen = mc.MIMOPilotChannel(pilot_alg,algConfig)
     (wp, vp) = pilgen.generatePilots(Nsym*K*Nrft, Na, Nd, Npr=Nsym*K*Nrfr,
                                     rShape=(Nsym, K, Nrfr, Na), tShape=(Nsym, K, Nd, Nrft))
     # print("wp=",wp)
     # print("vp=",vp)
-    # print("wp_shape=",wp.shape)
-    # print("vp_shape=",vp.shape)
+    print("wp_shape=",wp.shape)
+    print("vp_shape=",vp.shape)
